3ec7ccf4dd1eb780864544147110903d39ad789d
joshuaiyumva <joshuaiyumva@gmail.com>
2020-11-11 00:42:30 +0200

    add a leasable date format

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Quotes } from '../quotes';
 @Component({
   selector: 'app-quote-form',
@@ -7,18 +7,22 @@ import { Quotes } from '../quotes';
 })
 export class QuoteFormComponent implements OnInit {
 
-  // quotePublisher: string;
-  // quoteMessage: string;
-  // quoteAuthor: string;
-  // inputDate: Date;
-  // newMessage: any;
+  quotePublisher: string;
+  quoteMessage: string;
+  quoteAuthor: string;
+  inputDate: Date;
+  id:number;
+  newMessage: any;
 
-  // newquote = [
-  //   new Quotes(0, 'Josue', 'The greatest glory in living lies not in never falling, but in rising every time we fall.', 'Nelson Mandela', new Date())
+  newquote = [new Quotes(0, 'Josue', 'The greatest glory in living lies not in never falling, but in rising every time we fall.', 'Nelson Mandela', new Date(2020, 5, 12))];
+  @Output() Quotes = new EventEmitter<Quotes>(); 
 
-  // ]
-   
-
+  submitMessage(){
+    let quoteLength = this.newquote.length;
+    this.id = quoteLength + 1;
+    this.newMessage = new Quotes(0, this.quotePublisher, this.quoteMessage, this.quotePublisher, new Date());
+    this.newquote.push(this.newMessage);
+  }
 
   constructor() { }
 
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -15,12 +15,12 @@ export class QuoteComponent implements OnInit {
   newMessage: any;
 
   newquote = [
-    new Quotes(0, 'Josue', 'The greatest glory in living lies not in never falling, but in rising every time we fall.', 'Nelson Mandela', new Date()),
-    new Quotes(1,'Walt Disney', 'The way to get started is to quit talking and begin doing.', 'Eliane', new Date()),
-    new Quotes(2,'Steve Jobs', 'Your time is limited, so do not waste it living someone else life. Do not be trapped by dogma – which is living with the results of other peoples thinking.', 'Emile', new Date()),
-    new Quotes(3,'Oprah Winfrey', 'If you look at what you have in life, you will always have more.If you look at what you do not have in life, you will never have enough.', 'Emma', new Date()),
-    new Quotes(4,'James Cameron', 'If you set your goals ridiculously high and it is a failure, you will fail above everyone else success.', 'Josue', new Date()),
-    new Quotes(5, 'John Lennon', 'Life is what happens when you are busy making other plans', 'Josue', new Date())
+    new Quotes(0, 'Kelia', 'The greatest glory in living lies not in never falling, but in rising every time we fall.', 'Nelson Mandela', new Date(2020,10,14)),
+    new Quotes(1,'Walt Disney', 'The way to get started is to quit talking and begin doing.', 'Eliane', new Date(2020,3,14)),
+    new Quotes(2,'Steve Jobs', 'Your time is limited, so do not waste it living someone else life. Do not be trapped by dogma – which is living with the results of other peoples thinking.', 'Emile', new Date(2020,5,20)),
+    new Quotes(3,'Oprah Winfrey', 'If you look at what you have in life, you will always have more.If you look at what you do not have in life, you will never have enough.', 'Emma',new Date(2020,6,19)),
+    new Quotes(4,'James Cameron', 'If you set your goals ridiculously high and it is a failure, you will fail above everyone else success.', 'Josue', new Date(2020,6,5)),
+    new Quotes(5, 'John Lennon', 'Life is what happens when you are busy making other plans', 'Josue', new Date(2020,1,16))
   ];
 
   showMoreDetails(index){
